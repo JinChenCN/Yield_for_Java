0592bb4327067302a5eed8894b8787d35923b7ad
Jin Chen <chenjin10@gmail.com>
2016-03-21 21:06:09 +1300

    First commit. Yield works but still need to distinguish different yield in different methods.

---
+++ src/se701/StudentSample.java
@@ -0,0 +1,19 @@
+package se701;
+
+public class StudentSample {
+
+    public static void main(String[] args) {
+        sampleMethod();
+    }
+
+    public static void sampleMethod() {
+        System.out.println("Execution started");
+         {
+            for (int i = 0; i < 20; i++) {
+                System.out.println(i);
+            }
+        }
+        ;
+        System.out.println("Execution ended");
+    }
+}
